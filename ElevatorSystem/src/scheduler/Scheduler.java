1bda07f0f563f7223a6a4ad360a95ae3c192f1f2
CamSommer <CamSommerville@cmail.carleton.ca>
2022-01-24 09:23:14 -0500

    Created outline of what Scheduler must do for Iteration 1. No actual method implementation yet.

--- ElevatorSystem/src/scheduler/Scheduler.java
+++ ElevatorSystem/src/scheduler/Scheduler.java
@@ -1,5 +1,48 @@
 package scheduler;
 
-public class Scheduler {
+import java.util.*;
 
+import elevatorsubsystem.Elevator;
+import floorsubsystem.Floor;
+
+public class Scheduler {
+	
+	private List<Elevator> elevators;
+	private List<Floor> floors;
+	
+	public Scheduler() {
+		elevators = new ArrayList<Elevator>();
+	}
+	
+	public void addElevator(Elevator elevator) {
+		elevators.add(elevator);
+	}
+	
+	public void removeElevator(Elevator elevator) {
+		elevators.remove(elevator);
+	}
+	
+	public void receiveFloorInputs() {
+		//call corresponding floor methods
+	}
+	
+	private void updateFloorLamps() {
+		//send message to activate floor lamps
+	}
+	
+	public void receiveElevatorInputs() {
+		//call corresponding elevator methods
+	}
+	
+	private void updateElevatorLamps() {
+		
+	}
+	
+	private void activateElevatorMotor() {
+		
+	}
+	
+	private void activateElevatorDoor() {
+		
+	}
 }
