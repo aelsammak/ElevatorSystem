52c127297d2374bfba7210d1e41d5852e0e33bbb
aelsammak <adielsammak@cmail.carleton.ca>
2022-01-27 17:36:08 -0500

    Added changes to Common for file parsing

--- ElevatorSystem/src/common/Common.java
+++ ElevatorSystem/src/common/Common.java
@@ -1,7 +1,25 @@
 package common;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Common {
 	
-	public static int NUM_FLOORS = 20;
+	public static int NUM_FLOORS;
+	public static final SimpleDateFormat CSV_DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    public static Date SIMULATION_START_DATE;
+    
+    static {
+    	try {
+			SIMULATION_START_DATE = CSV_DATE_FORMAT.parse("01-01-2022 8:00:00");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public static void setMaxNumFloors(int maxFloors) {
+    	NUM_FLOORS = maxFloors;
+    }
 
 }
--- ElevatorSystem/src/elevatorsubsystem/ElevatorSubsystem.java
+++ ElevatorSystem/src/elevatorsubsystem/ElevatorSubsystem.java
@@ -1,5 +1,26 @@
 package elevatorsubsystem;
 
+import static java.util.stream.Collectors.groupingBy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+import FloorSubsystem.BottomFloor;
+import FloorSubsystem.Floor;
+import FloorSubsystem.MiddleFloor;
+import FloorSubsystem.TopFloor;
+import SchedulerSubsystem.Event;
+import SchedulerSubsystem.Scheduler;
+import scheduler.ElevatorEvent;
+
 public class ElevatorSubsystem {
+	
+    public static Queue<ElevatorEvent> generateElevatorEvents(Scheduler scheduler, String CSV_FILE_NAME) {
+    	
+        Queue<ElevatorEvent> elevatorEvents = new Q
+    }
 
 }
